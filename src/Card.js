378cd867d042256b59a2750ec5e0ebe601642e12
DESKTOP-U6O47B3\User <ayana.sultanovaa@gmail.com>
2022-01-15 19:15:21 +0300

    2

--- src/Card.js
+++ src/Card.js
@@ -6,47 +6,77 @@ import Typography from '@mui/material/Typography';
 import QRCode from "qrcode.react";
 import { Button } from '@mui/material';
 import './App.css';
-import { Link } from 'react-router-dom';
 import Modal from './Modal';
+import './Card.css'
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react';
+
+const theme = createTheme({
+  palette: {
+    neutral: {
+      main: '#fecc00',
+      contrastText: '#fff',
+    },
+  },
+});
 
 
 const PersonCard = () => {
-    const [info, setInfo] = useState([])
-    const [active, setActive ] = useState(false)
-    
+    const [inn, setInn] = useState('')
+    const [qrLink, setQrLink] = useState('')
+    const [fio, setFio] = useState('')
+    const [vaccines, setVaccines] = useState([])
+    const [active, setActive] = useState(false)
+    const [photo, setPhoto] = useState([])
+    const [numberId, setNumberId] = useState('')
+
+    // useEffect(() => {
+    //   axios.get('https://demo9363696.mockable.io/passport-info?phone=996772140014')
+    //   .then(res => {
+    //     const inn = res.data.inn
+    //     console.log(inn);
+    //     setInn(inn)
+    //   })
+    // }, [])
+    // console.log(inn);
+
     useEffect(()=> {
-        axios.get('http://localhost:3009/info', 
-        {
-          mode: 'no-cors',
-          withCredentials: true,
-          headers: {'Access-Control-Allow-Origin': '*'}
-        }).then(res => {
-            const info = res.data
-            setInfo( info )
+        axios.get(`https://ibank2.cbk.kg/minzdrav/covid-pass?phone=996772140014`, 
+        { mode: 'no-cors',
+        'Access-Control-Allow-Origin': '*'
+        }).catch(err =>console.log(err))
+          .then(res => {
+            const inn = res.data.passport.inn
+            const numberId = res.data.passport.numberId
+            const qrLink = res.data.propusk.qrLink
+            const fio = res.data.propusk.fio
+            const vaccines = res.data.propusk.vaccines
+            const photo = res.data.propusk.photo
+              setQrLink(qrLink)
+              setFio(fio)
+              setVaccines(vaccines)
+              setPhoto(photo)
+              setInn(inn)
+              setNumberId(numberId)
         })
     }, [])
-       
-
-    
-   
-        return <div> 
-           {info.map(info => <div> 
-            <Card sx={{ maxWidth: 345 }}>
      
+        return <div>            
+            <Card sx={{ maxWidth: 345 }}>
         <CardContent>
         <Typography variant="h5" component="div">
-           {info.fio}
+          <span> {fio} </span> 
           </Typography>
-        <img src={`data:image/jpeg;base64,${info.photo}`}/>
+        <img src={`data:image/jpeg;base64,${photo}`}  style={{ width: 110, height: 150 }}/>
         <Typography variant="body2" color="text.secondary">
-           {info.vaccines.map(v=> <div>
-               <p> {v.vaccine_title}: {v.vaccine_name}, {v.vaccination_date}</p>  
+           {vaccines.map(v=> <div key={v.doza}>
+               <span> {v.vaccine_title}: {v.vaccine_name}, {v.vaccination_date}</span>  
            </div>
            )}    
           </Typography>
         <div>
             <QRCode
-               value={info.qrLink} style={{ marginRight: 10, marginBottom: 20 }}
+               value={qrLink} style={{ marginRight: 10, marginBottom: 20, width: 200, height: 200 }}
                bgColor={"#ffffff"}
                fgColor={"#007d82"}
                imageSettings={{
@@ -59,18 +89,18 @@ const PersonCard = () => {
               }}
             />
          </div>
+         <ThemeProvider theme={theme}>
             <Button onClick={ () => setActive(true)} 
-                variant="contained" style={{paddingLeft: 13}} >
+                className='button'
+                variant="contained"
+                color="neutral"
+                 >
                 Получить сертификат
             </Button>
-            <Modal active={active} setActive={setActive} info={info}/>
-           
-        </CardContent>
-        
-      </Card>
-     
-        </div> 
-        )}    
+            </ThemeProvider>
+            <Modal active={active} setActive={setActive} numberId={numberId} inn={inn}/>           
+        </CardContent>        
+      </Card>     
      </div>
     }
 
--- src/Modal.js
+++ src/Modal.js
@@ -1,22 +1,40 @@
 import { Button, Link} from "@mui/material";
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import "./Modal.css"
 
-const Modal = ({active, setActive, info}) => {
+const Modal = ({active, setActive, inn, numberId}) => {
+    const [cer, setCer] = useState('')
+    // useEffect(()=> {
+    //     axios.get(`https://ibank2.cbk.kg/minzdrav/get-pdf-file?pin=${inn}&seriaId=ID&nomerId=${numberId}`,
+    //     {
+    //         mode: 'no-cors',
+    //         'Access-Control-Allow-Origin': '*'
+    //     })
+    //     .catch(err =>console.log(err))
+    //     .then(res => { 
+    //        const cer = res.data
+    //        setCer(cer)})
+    // })
+
+    const url = "https://ibank2.cbk.kg/minzdrav/get-pdf-file?pin=&{inn}\\&seriaId=ID\\&nomerId=&{nomerId}"
+
     return <div className={active? "modal active": "modal"} onClick={()=> setActive(false)}>
                 <div className="modalContent" onClick={e => e.stopPropagation()}> 
                 <p>На каком языке вы бы хотели получить сертификат?</p>
-               <div> <Link href={info.certificate}  target='_blank' rel='noopener noreferrer'>>
-                   <Button variant="contained" size="small" style={{marginBottom: '10px'}}> Русский / Кыргызский 
-                   </Button> </Link>                
+               <div> 
+                   <a href="https://ibank2.cbk.kg/minzdrav/get-pdf-file?pin=\\&{inn}&seriaId=ID&nomerId=\\&{nomerId}"  download>
+                   <Button size="small" style={{marginBottom: '10px'}}> Русский / Кыргызский 
+                   </Button> 
+                   </a>                
                </div> 
-                <div> <Link href={info.certificate}  target='_blank' rel='noopener noreferrer'>
-                    <Button variant="contained" size="small" style={{marginBottom: '10px'}}> English 
-                    </Button> </Link>
+                <div>
+                   <a href="https://ibank2.cbk.kg/minzdrav/get-pdf-file?pin=21610199201196&seriaId=ID&nomerId=0304674"  download>
+                    <Button  size="small" style={{marginBottom: '10px'}}>  Английский/English 
+                    </Button>
+                    </a>
                     </div> 
                 <div> 
-                    <Button variant="contained" size="small"  onClick={()=> setActive(false)}> Отмена 
+                    <Button  size="small"  onClick={()=> setActive(false)}> Отмена 
                     </Button> </div>
                  </div>
                 {/* <div> <NavLink to="cerEng">
