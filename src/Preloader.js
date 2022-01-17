21e387dca852d1dc182fcf3c41688c0ed1eb3c36
DESKTOP-U6O47B3\User <ayana.sultanovaa@gmail.com>
2022-01-17 16:56:58 +0300

    preloader was added

---
+++ src/Preloader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Preloader = () => {
+    return <div> <img src="https://i.ytimg.com/vi/bZ9Vzz8H-Lk/maxresdefault.jpg" style={{width: 300, height: 200 }} /> 
+                 <h2> Ayana </h2>
+                 </div>
+}
+
+export default Preloader
